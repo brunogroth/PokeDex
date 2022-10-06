30993cae77733e2c8a72adb8b549d65b61353fd0
10272129992 <brunom.groth@gmail.com>
2022-10-05 21:53:48 -0300

    First commit

---
+++ app/src/main/java/com/example/pokedex/adapter/PokeAdapter.kt
@@ -0,0 +1,9 @@
+package com.example.pokedex.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class PokeAdapter {
+
+    inner class PokeViewHolder(itemView:View):RecyclerView.ViewHolder(itemView)
+}
---
+++ app/src/main/java/com/example/pokedex/model/Pokemon.kt
@@ -0,0 +1,7 @@
+package com.example.pokedex.model
+
+data class Pokemon(
+    val id:String,
+    val name:String
+
+)
