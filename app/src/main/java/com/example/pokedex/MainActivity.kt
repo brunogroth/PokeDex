da6ebcca25a8d63902e78b1eaeb118191ab1c49a
10272129992 <brunom.groth@gmail.com>
2022-10-19 21:39:08 -0300

    Static Pokemon List Exibition

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.pokedex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pokedex.adapter.PokeAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        val recyclerView : RecyclerView = findViewById(R.id.poke_recycle);
+
+        recyclerView.layoutManager = GridLayoutManager(this, 3);
+
+        recyclerView.adapter = PokeAdapter()
     }
 }
--- app/src/main/java/com/example/pokedex/adapter/PokeAdapter.kt
+++ app/src/main/java/com/example/pokedex/adapter/PokeAdapter.kt
@@ -1,9 +1,37 @@
 package com.example.pokedex.adapter
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.View.inflate
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.content.res.ColorStateListInflaterCompat.inflate
+import androidx.core.content.res.ComplexColorCompat.inflate
 import androidx.recyclerview.widget.RecyclerView
+import com.example.pokedex.R
+import com.squareup.picasso.Picasso
 
-class PokeAdapter {
+class PokeAdapter : RecyclerView.Adapter<PokeAdapter.PokeViewHolder>() {
 
     inner class PokeViewHolder(itemView:View):RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokeViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+
+        val layout = layoutInflater.inflate(R.layout.poke_item_view, parent, false);
+        return PokeViewHolder(layout);
+    }
+
+    override fun onBindViewHolder(holder: PokeViewHolder, position: Int) {
+
+        val url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/39.png";
+
+        val imageView : ImageView = holder.itemView.findViewById(R.id.poke_image);
+
+        Picasso.get().load(url).into(imageView);
+    }
+
+    override fun getItemCount(): Int {
+        return 11;
+    }
 }
